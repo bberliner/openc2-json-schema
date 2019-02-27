4f404f014af997233e396ec7bfe2eb74be00aef9
Brian Berliner <Brian_Berliner@symantec.com>
2019-02-27 10:28:41 -0800

    Get initial Main up-and running.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,19 @@
 group = "com.brianberliner.openc2"
 version = "1.0-SNAPSHOT"
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+}
+
---
+++ src/main/kotlin/com/brianberliner/openc2/Main.kt
@@ -0,0 +1,5 @@
+package com.brianberliner.openc2
+
+fun main(args: Array<String>) {
+    println("Hello World.")
+}
