c0666d1bbf2ce68caf2b6a27d0c81f0736b7b777
Brian Berliner <Brian_Berliner@symantec.com>
2019-05-21 10:47:40 -0700

    Updated dependencies for Kotlin, Justify, and AssertK

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ group = "com.brianberliner.openc2"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.31"
     application
 }
 
@@ -16,14 +16,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    implementation("org.leadpony.justify", "justify", "0.12.0")
+    implementation("org.leadpony.justify", "justify", "0.16.0")
     implementation("org.glassfish", "javax.json", "1.1.4")
     implementation("com.github.ajalt", "clikt", "1.6.0")
 
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.4.0")
     testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.4.0")
     testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.4.0")
-    testImplementation("com.willowtreeapps.assertk", "assertk-jvm", "0.13")
+    testImplementation("com.willowtreeapps.assertk", "assertk-jvm", "0.16")
 }
 
 application.mainClassName = "com.brianberliner.openc2.MainKt"
