ee09764d1a06db53e076c7dfb027c99aba4d6d11
Brian Berliner <Brian_Berliner@symantec.com>
2019-09-16 14:20:19 -0700

    Update all dependencies to latest.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ group = "com.brianberliner.openc2"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
     application
 }
 
@@ -16,14 +16,14 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
 
-    implementation("org.leadpony.justify", "justify", "1.0.0")
+    implementation("org.leadpony.justify", "justify", "1.1.0")
     implementation("org.glassfish", "javax.json", "1.1.4")
     implementation("com.github.ajalt", "clikt", "2.1.0")
 
-    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.5.1")
-    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.5.1")
-    testImplementation("com.willowtreeapps.assertk", "assertk-jvm", "0.19")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-params", "5.5.2")
+    testImplementation("com.willowtreeapps.assertk", "assertk-jvm", "0.20")
 }
 
 application.mainClassName = "com.brianberliner.openc2.MainKt"
